98db0e86b5d30ff4de11db338b90ab45d2775c2b
Biki-dev <sumikalita99999999@gmail.com>
2025-09-29 20:23:15 +0530

    signup controller add

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 const app = express();
+app.use(express.json());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages", messageRoutes);
-app.listen(PORT, () => console.log("server is runing"));
+app.listen(PORT, () => {
+    console.log("server is runing")
+    connectDB()
+});
