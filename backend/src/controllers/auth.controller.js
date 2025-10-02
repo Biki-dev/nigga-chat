6fb5babb2677c040085073f735b22a80ed0b9b14
Biki-dev <sumikalita99999999@gmail.com>
2025-10-02 16:16:22 +0530

    login and logout functions

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -46,3 +46,36 @@ export const signup = async (req,res) => {
     }
 
 }
+
+export const login = async (req,res) => {
+    const {email,password} = req.body
+
+    try {
+        if(!email || !password){
+            return res.status(400).json({message:"All fields are required"})
+        }
+
+        const user = await User.findOne({email})
+        if(!user) return res.status(400).json({message:"User not found"})
+
+        const isMatch = await bcrypt.compare(password,user.password)
+        if(!isMatch) return res.status(400).json({message:"Invalid credentials"})
+
+        generateToken(user._id,res)
+
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            email:user.email,
+            profilePic:user.profilePic,
+        })
+    } catch (error) {
+        console.log("Error in login controller",error)
+        res.status(500).json({message:"Internal server error"})
+    }
+}
+
+export const logout = (_,res) => {
+    res.cookie("jwt","",{maxAge:0})
+    res.status(200).json({message:"Logged out successfully"})
+}
