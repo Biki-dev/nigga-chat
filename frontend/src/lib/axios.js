8b7627d3df095a5756314d8a87e4151ec8f6d223
Biki-dev <sumikalita99999999@gmail.com>
2025-10-04 20:39:59 +0530

    ok

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:4000" // local backend when developing
+      ? "http://localhost:4000/api" // local backend when developing
       : import.meta.env.VITE_API_URL, // backend URL on Render
   withCredentials: true,
 });
