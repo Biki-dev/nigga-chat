5591194b588ed111468c0d8279750739da464361
Biki-dev <sumikalita99999999@gmail.com>
2025-10-04 14:09:08 +0530

    half ready

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,7 +1,7 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
 import cloudinary from "../lib/cloudinary.js";
-
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getAllContacts = async (req, res) => {
     try {
@@ -36,6 +36,17 @@ export const sendMessage = async (req, res) => {
         const { id: receiverId } = req.params;
         const { text, image } = req.body;
 
+        if (!text && !image) {
+      return res.status(400).json({ message: "Text or image is required." });
+    }
+    if (senderId.equals(receiverId)) {
+      return res.status(400).json({ message: "Cannot send messages to yourself." });
+    }
+    const receiverExists = await User.exists({ _id: receiverId });
+    if (!receiverExists) {
+      return res.status(404).json({ message: "Receiver not found." });
+    }
+
         let ImageUrl;
         if (image) {
             const uploadResponse = await cloudinary.uploader.upload(image);
@@ -50,9 +61,16 @@ export const sendMessage = async (req, res) => {
         });
         await newMessage.save();
 
+        // Notify the receiver via socket
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
+
         res.status(201).json(newMessage);
     } catch (error) {
-        res.status(500).json({ message: "Server Error" });
+        console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
     }
 };
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,24 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
 dotenv.config();
+import { app, server } from "./lib/socket.js";
+
+
 const PORT = process.env.PORT || 4000;
-const app = express();
-app.use(express.json());
+
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+
 app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages", messageRoutes);
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("server is runing")
     connectDB()
 });
