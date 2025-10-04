000fdae96b3e4c1bd9b6a8e865ee6487cafce23c
Biki-dev <sumikalita99999999@gmail.com>
2025-10-04 20:57:02 +0530

    hm

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,8 +9,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [process.env.CLIENT_URL],
-    credentials: true,
+      origin: "https://nigga-chat.vercel.app", // frontend URL
+    methods: ["GET", "POST"],
+    credentials: true, 
   },
 });
 
